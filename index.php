b0120ce0dcfcf4b9af2b38cebef9bd23b081b213
gokhan <gokhan@192.168.1.159>
2013-06-05 16:11:56 +0300

    Kardeş yazılım danışmanlık hizmetleri berd (bro erp root design)

--- index.php
+++ index.php
@@ -6,6 +6,12 @@
  * 05.Haz.2013-16:03:25 
  * index
  */
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
 
+//route 
+$q = isset($_GET ['q']) ? $_GET ['q'] : null;
+$q = rtrim($q, '/');
+$q = explode('/', $q);
 
 ?>
